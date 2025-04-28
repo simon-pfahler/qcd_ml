5b0f3111688e3895ffb2d11addec1101c50e5e68
Rielco <57397178+CostantiniRiccardo@users.noreply.github.com>
2025-04-28 14:37:31 +0200

    Bugfix and update the weight and hermitian matrices

--- src/qcd_ml/nn/matrix_layers/exponentiation.py
+++ src/qcd_ml/nn/matrix_layers/exponentiation.py
@@ -11,20 +11,46 @@ class LGE_Exp(torch.nn.Module):
     gauge links, i.e.,
 
     .. math::
-        U_\mu(x) \rightarrow \exp\left(i\sum\limits_j \beta_{\mu,i} W_{i}(x)\right) U_\mu(x)
+        U_\mu(x) \rightarrow \exp\left(\sum\limits_j \beta_{\mu,i} W_{i}(x)\right) U_\mu(x)
     """
-    def __init__(self, n_features_in):
+
+    def __init__(self, n_features_in, matrix_mode='Ah'):
+        """
+
+        :param n_features_in:
+        :param matrix_mode: Type of anti-hermitian matrix used in the exponent
+        Ah -> exp(anti-hermitian)
+        H -> exp(i * hermitian)
+        Ah_H -> exp(anti-hermitian + i * hermitian)
+        """
         super(LGE_Exp, self).__init__()
-        self.weights = torch.nn.Parameter(torch.randn(4, n_features_in, dtype=torch.cdouble))
-        
+        self.matrix_mode = matrix_mode
+        if matrix_mode == "Ah" or matrix_mode == "Ah_H":
+            self.ah_weights = torch.nn.Parameter(torch.randn(4, n_features_in, dtype=torch.double))
+        if matrix_mode == "H" or matrix_mode == "Ah_H":
+            self.h_weights = torch.nn.Parameter(torch.randn(4, n_features_in, dtype=torch.double))
+        if matrix_mode not in ["Ah", "H", "Ah_H"]:
+            raise Exception("invalid matrix_mode. The possibilities are:\n"
+                            "Ah -> exp(anti-hermitian)\n"
+                            "H -> exp(i * hermitian)\n"
+                            "Ah_H -> exp(anti-hermitian + i * hermitian)")
+
     def forward(self, U, W):
-        hermitian = W.adjoint() - W
-        trace = torch.einsum("mabcdii->mabcd", hermitian)
+        transform_matrix = torch.zeros(U.shape, dtype=torch.cdouble)
+        W_adj = W.adjoint()
 
-        identity = torch.clone(hermitian)
-        identity[:,:,:,:,:] = torch.eye(3,3, dtype=torch.cdouble)
-        trace_removal = torch.einsum("mabcdij,mabcd->mabcdij", identity, trace)
-        traceless_hermitian = 1j/2 * (hermitian -  trace_removal / 3)
+        if self.matrix_mode == "Ah" or self.matrix_mode == "Ah_H":
+            anti_hermitian = W - W_adj
+            traceless_anti_hermitian = anti_hermitian - torch.einsum("itxyzaa,bc->itxyzbc", anti_hermitian, torch.eye(3)) / 3
 
-        transform_matrix = torch.matrix_exp(1j * torch.einsum("ui,i...->u...", self.weights, traceless_hermitian))
+            transform_matrix += torch.matrix_exp(torch.einsum("ui,i...->u...",
+                                                              1j * self.ah_weights, -1j * traceless_anti_hermitian))
+
+        if self.matrix_mode == "H" or self.matrix_mode == "Ah_H":
+            hermitian = W + W_adj
+            traceless_hermitian = hermitian - torch.einsum("itxyzaa,bc->itxyzbc", hermitian, torch.eye(3)) / 3
+
+            transform_matrix += torch.matrix_exp(torch.einsum("ui,i...->u...",
+                                                              1j * self.h_weights, -1j * traceless_hermitian))
+
         return torch.einsum("uabcdij,uabcdjk->uabcdik", transform_matrix, U)
