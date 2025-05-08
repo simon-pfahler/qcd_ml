9f7b1eb9d4357f7148fde6cda5f09166217e0374
Rielco <57397178+CostantiniRiccardo@users.noreply.github.com>
2025-05-08 16:42:00 +0200

    Update exponentiation.py

--- src/qcd_ml/nn/matrix_layers/exponentiation.py
+++ src/qcd_ml/nn/matrix_layers/exponentiation.py
@@ -14,39 +14,39 @@ class LGE_Exp(torch.nn.Module):
         U_\mu(x) \rightarrow \exp\left(\sum\limits_j \beta_{\mu,i} W_{i}(x)\right) U_\mu(x)
     """
 
-    def __init__(self, n_features_in, matrix_mode='Ah'):
+    def __init__(self, n_features_in, matrix_mode='a'):
         """
 
         :param n_features_in:
         :param matrix_mode: Type of anti-hermitian matrix used in the exponent
-        Ah -> exp(anti-hermitian)
-        H -> exp(i * hermitian)
-        Ah_H -> exp(anti-hermitian + i * hermitian)
+        a -> exp(anti-hermitian)
+        h -> exp(i * hermitian)
+        ah -> exp(anti-hermitian + i * hermitian)
         """
         super(LGE_Exp, self).__init__()
         self.matrix_mode = matrix_mode
-        if matrix_mode == "Ah" or matrix_mode == "Ah_H":
+        if matrix_mode == "a" or matrix_mode == "ah":
             self.ah_weights = torch.nn.Parameter(torch.randn(4, n_features_in, dtype=torch.double))
-        if matrix_mode == "H" or matrix_mode == "Ah_H":
+        if matrix_mode == "h" or matrix_mode == "ah":
             self.h_weights = torch.nn.Parameter(torch.randn(4, n_features_in, dtype=torch.double))
-        if matrix_mode not in ["Ah", "H", "Ah_H"]:
-            raise Exception("invalid matrix_mode. The possibilities are:\n"
-                            "Ah -> exp(anti-hermitian)\n"
-                            "H -> exp(i * hermitian)\n"
-                            "Ah_H -> exp(anti-hermitian + i * hermitian)")
+        if matrix_mode not in ["a", "h", "ah"]:
+            raise ValueError("invalid matrix_mode. The possibilities are:\n"
+                            "a -> exp(anti-hermitian)\n"
+                            "h -> exp(i * hermitian)\n"
+                            "ah -> exp(anti-hermitian + i * hermitian)")
 
     def forward(self, U, W):
         transform_matrix = torch.zeros(U.shape, dtype=torch.cdouble)
         W_adj = W.adjoint()
 
-        if self.matrix_mode == "Ah" or self.matrix_mode == "Ah_H":
+        if self.matrix_mode == "a" or self.matrix_mode == "ah":
             anti_hermitian = W - W_adj
             traceless_anti_hermitian = anti_hermitian - torch.einsum("itxyzaa,bc->itxyzbc", anti_hermitian, torch.eye(3)) / 3
 
             transform_matrix += torch.matrix_exp(torch.einsum("ui,i...->u...",
                                                               1j * self.ah_weights, -1j * traceless_anti_hermitian))
 
-        if self.matrix_mode == "H" or self.matrix_mode == "Ah_H":
+        if self.matrix_mode == "h" or self.matrix_mode == "ah":
             hermitian = W + W_adj
             traceless_hermitian = hermitian - torch.einsum("itxyzaa,bc->itxyzbc", hermitian, torch.eye(3)) / 3
 
